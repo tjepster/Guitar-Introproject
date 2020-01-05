98a6a30a59b77fbe1376b4854fb8c37c70f03cca
Koenvisser <KoenVisser2000@gmail.com>
2020-01-05 10:13:16 +0100

    Added support for multiple levels

--- Assets/scripts/LevelSelectScript.cs
+++ Assets/scripts/LevelSelectScript.cs
@@ -1,13 +1,36 @@
-﻿using System.Collections;
+﻿using System.IO;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class LevelSelectScript : MonoBehaviour
 {
-    public void PlayLevel1()
+    public GameObject myPrefab;
+    bool Executed = false;
+    // This script will simply instantiate the Prefab when the game starts.
+    public void AddText()
     {
-        PlayerPrefs.SetString("currentLevel", "level1");
+        if (Executed == false)
+        {
+            int posy = 80;
+            string[] directories = Directory.GetDirectories(Application.dataPath + "/levels");
+            foreach (string dir in directories)
+            {
+                string dir2 = dir.Replace(Application.dataPath + "/levels\\", "");
+                GameObject button = Instantiate(myPrefab, new Vector3(transform.position.x, transform.position.y + posy, transform.position.z), Quaternion.identity, transform);
+                button.transform.GetChild(0).GetComponent<TextMeshProUGUI>().SetText(dir2);
+                button.GetComponent<Button>().onClick.AddListener(() => PlayLevel(dir2));
+                posy -= 50;
+            }
+            Executed = true;
+        }
+    }
+    public void PlayLevel(string level)
+    {
+        PlayerPrefs.SetString("currentLevel", level);
         SceneManager.LoadScene("Level 1");
     }
 
