e70d2022bb5cd97dfca7c1504eebfd556800e31d
Koenvisser <KoenVisser2000@gmail.com>
2020-01-07 22:18:29 +0100

    Started working on level editor

--- Assets/scripts/LevelSelectScript.cs
+++ Assets/scripts/LevelSelectScript.cs
@@ -22,17 +22,28 @@ public class LevelSelectScript : MonoBehaviour
             {
                 if(File.Exists(dir + "/level.txt") && File.Exists(dir + "/song.wav"))
                 {
+                    //get name of level
                     string dir2 = dir.Replace(Application.dataPath + "/levels\\", "");
+
+                    //create button
                     GameObject button = Instantiate(myPrefab, new Vector3(transform.position.x, transform.position.y + posy, transform.position.z), Quaternion.identity, transform);
+
+                    //set the text of the button to the name of the level
                     button.transform.GetChild(0).GetComponent<TextMeshProUGUI>().SetText(dir2);
+
+                    //add an onclick event to the button to play the level when the button is clicked
                     button.GetComponent<Button>().onClick.AddListener(() => PlayLevel(dir2));
 
+                    //create eventtriggers for pointerenter and pointerexit for the button to display the highscores for the level when hovering over the button, 
+                    //and displaying the main highscores when not hovering over the button anymore
                     EventTrigger.Entry eventtype = new EventTrigger.Entry();
                     eventtype.eventID = EventTriggerType.PointerEnter;
                     eventtype.callback.AddListener((eventData) => {StartCoroutine(gameObject.GetComponent<ScoreSetGet>().GetScores(dir2));});
                     EventTrigger.Entry eventtype2 = new EventTrigger.Entry();
                     eventtype2.eventID = EventTriggerType.PointerExit;
                     eventtype2.callback.AddListener((eventData) => { StartCoroutine(gameObject.GetComponent<ScoreSetGet>().GetScores("")); });
+
+                    //add the eventtriggers to the button
                     button.AddComponent<EventTrigger>();
                     button.GetComponent<EventTrigger>().triggers.Add(eventtype);
                     button.GetComponent<EventTrigger>().triggers.Add(eventtype2);
