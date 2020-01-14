2e8ab2938d33125fa2eef2a9f296cec35ce7ba5e
Koenvisser <57984909+Koenvisser@users.noreply.github.com>
2020-01-14 17:01:31 +0100

    working on level editor

--- Assets/scripts/LevelEditor.cs
+++ Assets/scripts/LevelEditor.cs
@@ -14,7 +14,9 @@ public class LevelEditor : MonoBehaviour
     public GameObject BluePrefab;
     public GameObject ContentView;
     public GameObject AudioBar;
+    public GameObject AudioLine;
     public GameObject InputField;
+    public GameObject scrollbar;
     public AudioSource song;
 
     // Start is called before the first frame update
@@ -39,36 +41,52 @@ public class LevelEditor : MonoBehaviour
                 SetWidth(songlength);
             }
         }
-       }
+    }
 
-    void SetWidth(float songlength) {
+    private void SetWidth(float songlength) {
         RectTransform rt = ContentView.GetComponent<RectTransform>();
         rt.offsetMax = new Vector2(songlength * 120 - 648, rt.offsetMax.y);
     }
 
-    public void OnEditorClick() {
-        Vector3 vector3 = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -1);
-        Vector3 mouseposition = Camera.main.ScreenToWorldPoint(vector3);
-        if (mouseposition.x < 0.95 && mouseposition.x > -0.63)
-        {
-            if (mouseposition.y > 0.45 && mouseposition.y < 0.79)
-            {
-                Debug.Log(1);
-            }
-            else if (mouseposition.y > 0.81 && mouseposition.y < 1.14)
-            {
-                Debug.Log(2);
-            }
-            else if (mouseposition.y > 1.165 && mouseposition.y < 1.52)
-            {
-                Debug.Log(3);
-            }
-        }
+    public void OnYellowClick() 
+    {
+        OnContentClick(1);
+
     }
 
+    public void OnRedClick()
+    {
+        OnContentClick(2);
+
+    }
+
+    public void OnBlueClick()
+    {
+        OnContentClick(3);
+    }
+
+    private void OnContentClick(int colour)
+    {
+        Vector3 mouseposition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -1);
+        float left = ContentView.GetComponent<RectTransform>().sizeDelta.x * scrollbar.GetComponent<Scrollbar>().value + mouseposition.x - 135;
+    }
     public void AddAudio() {
         string songlocation = InputField.GetComponent<TMP_InputField>().text;
         StartCoroutine(ImportAudio(songlocation,song));
 
+    }
+
+    public void PlayAudio()
+    { song.Play(); }
+
+    public void PauseAudio()
+    { song.Pause(); }
+
+    private void Update()
+    {
+        if (song.isPlaying)
+        {
+            AudioLine.GetComponent<RectTransform>().localPosition = new Vector2(song.time * 120, AudioLine.GetComponent<RectTransform>().localPosition.y) ;
+        }
     }
 }
